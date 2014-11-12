e750528e74bf41e03a2d3cc7cf4e66506b35a24d
Jonathan Bennett <jonathan@jbennett.me>
2014-11-12 14:58:28 -0500

    correct issue with articles with the same slug from different issues being incorrectly selected

--- helpers.js
+++ helpers.js
@@ -54,6 +54,26 @@ var helpers = {
 		return { issueNum: issue.getNumber('issue.issue_number'), slug: article.getText('article.slug') };
 	},
 
+	/**
+	 * returns the first article from the articles with an id found in the list of linked documents
+	 *
+	 * note: if issues are able to link to more than just articles, this will need 
+	 * to be changed to only search the issue.articles array
+	 **/
+	articleInIssue: function(articles, issue) {
+		var issueArticleIDs = issue.linkedDocuments.map(function(doc) {
+			return doc.id;
+		});
+
+		for (var i = 0; i < articles.length; i++) {
+			if (issueArticleIDs.indexOf(articles[i].id) > -1) {
+				return articles[i];
+			}
+		}
+
+		throw "No article found in issue";
+	},
+
 	// Issue related
 	issueLabel: function(issue) {
 		return 'Issue '+ issue.getText('issue.issue_number') +', '+ issue.getText('issue.issue_date');
