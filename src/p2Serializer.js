acc1fb5e645f2c163228add27b5f06696b104f8c
Jonathan Bennett <jonathan@jbennett.me>
2014-11-12 20:00:38 -0500

    basic label processing

--- src/p2Serializer.js
+++ src/p2Serializer.js
@@ -1,13 +1,17 @@
 module.exports = function(element, content) {
+    if (element.type == 'label') {
+        if (element.data.label == 'code') {
+            return '<span class="inline-code"><code>'+ content +'</code></span>';
+        }
+    }
+
 	if (element.label) {
 		if (element.label == 'blockquote' && element.type === 'paragraph') {
 			return '<blockquote><p>'+ content +'</p></blockquote>';
-		} else {
-			// console.log(element);
-			// console.log(content);
+		} else if (element.label == 'code') {
+			return '<pre><code class="gist">'+ content +'</code></pre>';
 		}
 	}
-
 
 	return null;
 }
