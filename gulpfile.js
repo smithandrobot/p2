678c8066a0957afe78545d6a60efc05d9b04d840
Jonathan Bennett <jonathan@jbennett.me>
2014-11-13 14:38:03 -0500

    site build

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var baked = require('baked/gulp');
-require('./helpers.js');
-
-gulp.task('serve', ['baked:serve']);
-gulp.task('default', ['baked:default']);
-gulp.task('clean', ['baked:clean']);
