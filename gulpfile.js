1a81f30b8d9b02491fee64b1f9a4488cbc0d7f6b
Jonathan Bennett <jonathan@jbennett.me>
2014-11-17 11:14:56 -0500

    include potato.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var baked = require('baked/gulp');
+var potato = require('potato.js');
 require('./helpers.js');
+helpers.potato = potato;
 
 gulp.task('serve', ['baked:serve']);
 gulp.task('default', ['baked:default']);
